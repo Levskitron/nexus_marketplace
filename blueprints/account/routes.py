91dca79f5563005556fc20d6c76bef9a88be6a18
Levi <Levski247@outlook.com>
2025-11-14 01:42:04 +0000

    fixed routes and navbar

--- blueprints/account/routes.py
+++ blueprints/account/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template
 from . import account_bp
 
-@account_bp.route("/")
-def my_account():
-    return render_template("account/my_account.html")
+@account_bp.route("/account")
+def account():
+    return render_template("account/account.html")
 
 @account_bp.route("/orders")
 def order_history():
