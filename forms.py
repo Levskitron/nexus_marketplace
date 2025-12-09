b1ff84d93dbe3cb843f00ba0c6919e0a4518645e
Levi <Levski247@outlook.com>
2025-12-09 16:02:57 +0000

    re-done forms.py for image uploading

--- forms.py
+++ forms.py
@@ -1,26 +1,31 @@
-# forms.py
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, IntegerField, SelectField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Length, NumberRange, Optional, URL
+from wtforms import StringField, TextAreaField, DecimalField, SelectField, SubmitField
+from wtforms.validators import DataRequired, NumberRange, Optional
+from flask_wtf.file import FileField, FileAllowed
+
 
 class ProductForm(FlaskForm):
-    name = StringField("Product Name", validators=[DataRequired(), Length(max=100)])
-    description = TextAreaField("Description", validators=[Optional(), Length(max=250)])
-    brand = StringField("Brand", validators=[Optional(), Length(max=100)])
-
-    price = DecimalField("Price (£)", places=2,
-                         validators=[DataRequired(), NumberRange(min=0)])
-    stock_quantity = IntegerField("Stock Quantity",
-                                  validators=[DataRequired(), NumberRange(min=0)])
+    name = StringField("Name", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[Optional()])
+    price = DecimalField("Price (£)", validators=[DataRequired(), NumberRange(min=0)])
+    brand = StringField("Brand", validators=[Optional()])
 
     condition = SelectField(
         "Condition",
-        choices=[("new", "New"), ("used", "Used"), ("refurbished", "Refurbished")],
-        validators=[Optional()],
+        choices=[
+            ("new", "New"),
+            ("used", "Used"),
+            ("refurbished", "Refurbished"),
+        ],
+        validators=[DataRequired()],
     )
 
-    image_url = StringField("Image URL", validators=[Optional(), Length(max=255)])
-    category_id = SelectField("Category", coerce=int, validators=[Optional()])
+    image = FileField(
+        "Product Image",
+        validators=[
+            Optional(),
+            FileAllowed(["jpg", "jpeg", "png", "webp"], "Images only!")
+        ],
+    )
 
-    submit = SubmitField("Save Product")
+    submit = SubmitField("Save")
